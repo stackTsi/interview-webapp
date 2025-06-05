9f6767b2311709dfac14a020f07a869202c47735
stackTsi <dashzero123@gmail.com>
2025-06-05 11:03:04 +0700

    added a method in QuestionRepo.java

--- interview-webapp/src/main/java/com/example/interviewWebapp/Repository/QuestionRepo.java
+++ interview-webapp/src/main/java/com/example/interviewWebapp/Repository/QuestionRepo.java
@@ -1,11 +1,16 @@
 package com.example.interviewWebapp.Repository;
 
+import com.example.interviewWebapp.Entity.Enum.Category;
+import com.example.interviewWebapp.Entity.Enum.Level;
 import com.example.interviewWebapp.Entity.Questions;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepo extends MongoRepository<Questions, ObjectId> {
+    List<Questions> findByLevelAndCategory(Level level, Category category);
     //find by objectID implementation here
 }
