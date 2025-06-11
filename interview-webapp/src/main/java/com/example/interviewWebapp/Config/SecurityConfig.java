2a8db90b1a0c3eb508f4031a4348820fcf00afd1
stackTSi <dashzero123@gmail.com>
2025-06-11 07:15:08 +0700

    clean up some comments and changes.

--- interview-webapp/src/main/java/com/example/interviewWebapp/Config/SecurityConfig.java
+++ interview-webapp/src/main/java/com/example/interviewWebapp/Config/SecurityConfig.java
@@ -30,9 +30,10 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager customAuthenticationManager(HttpSecurity http) throws Exception{
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception{
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(userDetailsService)
+        authenticationManagerBuilder
+                .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder);
         return authenticationManagerBuilder.build();
     }
